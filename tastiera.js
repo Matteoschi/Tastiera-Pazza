f2d764e2969f110a799043f9eb050d7c379fe637
Matteo <94646702+Matteoschi@users.noreply.github.com>
2022-12-28 13:34:04 +0100

    Add files via upload

---
+++ tastiera.js
@@ -0,0 +1,54 @@
+const currentTitle = document.title;
+window.addEventListener("blur",() => {
+  document.title="Torna Presto";
+});
+window.addEventListener("focus",() =>{
+  document.title=currentTitle;
+});
+
+const audioe=new Audio();
+audioe.src="./ehm.mp3";
+const audior=new Audio();
+audior.src="./representation.mp3";
+const audioc=new Audio();
+audioc.src="./audio_CRAI.mp3";
+const audiod=new Audio();
+audiod.src="./destroyed.mp3";
+const audion=new Audio();
+audion.src="./nain.mp3";
+const audiol=new Audio();
+audiol.src="./lanch.mp3";
+const audiog=new Audio();
+audiog.src="./genious.mp3";
+const audiof=new Audio();
+audiof.src="./flag.wav";
+const audiom=new Audio();
+audiom.src="./mona.mp3";
+const audiospace=new Audio();
+audiospace.src="./SHISH.mp3";
+const audiot=new Audio();
+audiot.src="./time.wav";
+const audiob=new Audio();
+audiob.src="./brevetto.mp3";
+const audios=new Audio();
+audios.src="./speaking.wav";
+const audioi=new Audio();
+audioi.src="./idea.wav";
+const audiop=new Audio();
+audiop.src="./possibility.wav";
+const audioa=new Audio();
+audioa.src="./also.wav";
+const audioo=new Audio();
+audioo.src="./oo.wav";
+const audioq=new Audio();
+audioq.src="./question.wav";
+const audioh=new Audio();
+audioh.src="./has.wav";
+const audiou=new Audio();
+audiou.src="./uk.wav";
+const audio=new Audio();
+audio.src="./SHISH.mp3";
+const audioalt=new Audio();
+audioalt.src="./alt.mp3";
+console.log("ciao zi spero che te piaccia sto sito")
+alert("SHISH")
